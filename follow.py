769502fc482eb9596caacb56be315582780d2036
parthasarathis <parthasarathi_s@tce.edu>
2024-04-09 23:58:18 +0530

    hel

--- follow.py
+++ follow.py
@@ -4,6 +4,14 @@ import time
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import numpy as np
+from scipy.signal import argrelextrema
+import mplfinance as mpf
+
+
+Time_frame = '15m'
+limit = 100
+
 
 # Replace YOUR_API_KEY and YOUR_SECRET_KEY with your actual Binance API credentials
 API_KEY = "FLPeS5t9QAntmcsEtYYv01JBZufM5SQQu751qh0s9rcHwVXZqioztxpMnyVMxvlP"
@@ -18,7 +26,7 @@ symbol = 'ADAUSDT'
 # Function to fetch klines (candlestick) data from Binance API
 
 
-def get_klines_data(interval='15m', limit=1000):
+def get_klines_data(interval='1h', limit=1000):
     params = {'symbol': symbol, 'interval': interval, 'limit': limit}
     response = requests.get(url, params=params)
 
@@ -29,7 +37,6 @@ def get_klines_data(interval='15m', limit=1000):
                                          'taker_buy_quote_asset_volume', 'ignore'])
         # Convert timestamp to datetime format
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        df.set_index('timestamp', inplace=True)
         df['close'] = df['close'].astype(float)
         df['high'] = df['high'].astype(float)
         df['low'] = df['low'].astype(float)
@@ -45,13 +52,63 @@ def get_klines_data(interval='15m', limit=1000):
 # Main loop for data collection and time series analysis
 if __name__ == "__main__":
     # Initialize empty lists to store actual and forecasted BTC prices
-    data = get_klines_data(interval='15m', limit=100)
+    data = get_klines_data(interval=Time_frame, limit=limit)
+
+    # Convert 'timestamp' column to datetime and set as index
+    data['timestamp'] = pd.to_datetime(data['timestamp'])
+    # print(data['timestamp'])
+    data.set_index('timestamp', inplace=True)
+
+    # Find local maxima
+    local_max = argrelextrema(np.array(data['high']), np.greater)[0]
+    local_min = argrelextrema(np.array(data['low']), np.less)[0]
+
+    local_extrema = np.sort(np.concatenate((local_max, local_min))).tolist()
+
+    # Create a new figure and an axes
+    fig, ax = plt.subplots()
+
     # Plotting high and low prices
-    plt.plot(data['high'], label='High')
-    plt.plot(data['low'], label='Low')
+    ax.plot(data['high'], label='High')
+    ax.plot(data['low'], label='Low')
 
-    # plt.bar(data.index, data['volume'], label='Volume')
+    local_max_df = pd.DataFrame({
+        'local_max': data['high'].iloc[local_max]
+    })  # Create a DataFrame for local maxima
 
-    plt.legend()
+    # print(local_max_df)
+
+    local_min_df = pd.DataFrame({
+        'local_min': data['low'].iloc[local_min]
+    })  # Create a DataFrame for local minima
+
+    # print(local_min_df)
+    # local_extrema_df = pd.concat(
+    # [local_max_df, local_min_df]).sort_values('timestamp')
+
+    local_extrema_df = pd.merge(
+        local_max_df, local_min_df, on='timestamp', how='outer', sort=True,)
+
+    # local_extrema_df.fillna(0, inplace=True)
+
+    local_extrema_df['local_extrema'] = local_extrema_df['local_max'] + \
+        local_extrema_df['local_min']
+
+    print(local_extrema_df)
+
+    # plt.plot(local_extrema_df.index, local_extrema_df['local_extrema'])
+    plt.xlabel('Timestamp')
+    plt.ylabel('Local Extrema')
+    plt.title('Timestamp vs Local Extrema')
+
+    ax.scatter(data.index[local_max],
+               data['high'].iloc[local_max], color='r', label='local max')
+    ax.scatter(data.index[local_min],
+               data['low'].iloc[local_min], color='g', label='local min')
+
+    ax.legend()
     plt.xticks(rotation=45)  # Rotate x-axis labels by 45 degrees
+
+    # Plot candlestick chart
+    # mpf.plot(data, type='candle', ax=ax)
     plt.show()
