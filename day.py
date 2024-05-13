064b1a58c128920e61f1405b93a20b8c412198fa
parthasarathis <parthasarathi_s@tce.edu>
2024-05-13 15:13:18 +0530

    hello

--- day.py
+++ day.py
@@ -20,7 +20,23 @@ coin_price = collections.deque([], maxlen=20)
 
 futures_type = FuturesType.USD_M
 
-table_placeholder = st.empty()
+st.set_page_config(layout='wide')
+pd.set_option('display.max_rows', 20)
+
+
+up_col, down_col, up1_col, down1_col = st.columns(4)
+
+up_placeholder = up_col.empty()
+down_placeholder = down_col.empty()
+up1_placeholder = up1_col.empty()
+down1_placeholder = down1_col.empty()
+
+
+up_title = st.empty()
+down_title = st.empty()
+up1_title = st.empty()
+down1_title = st.empty()
+
 
 # write comments for below function
 
@@ -62,11 +78,14 @@ def get_tickers(partha_account, yesterday_df):
     table_data['D_C_MIN'] = table_data['D_C_MIN'].round(3)
     table_data['D_C_MAX'] = table_data['D_C_MAX'].round(3)
 
+    actual_df = table_data[['symbol', 'D_CHANGE', 'D_OPEN', 'price']]
+    actual_df = actual_df.sort_values('D_CHANGE', ascending=False)
+
     day_df = table_data[['symbol', 'D_MAX', 'D_MIN', 'D_C_MAX', 'D_C_MIN']]
     day_df = day_df[day_df['D_MAX'] != 0]
 
     day_df = day_df.sort_values('D_C_MIN', ascending=False)
-    print(tabulate(day_df, headers='keys', tablefmt='psql'))
+    # print(tabulate(day_df, headers='keys', tablefmt='psql'))
     up_df = day_df[day_df['D_MIN'] > - 1]
     up_df = up_df.sort_values('D_C_MIN', ascending=False)
     down_df = day_df[day_df['D_MAX'] < 1]
@@ -74,9 +93,11 @@ def get_tickers(partha_account, yesterday_df):
 
     up1_df = day_df
     up1_df = up1_df.sort_values('D_C_MAX', ascending=False)
+    up1_df.set_index('symbol', inplace=True)
     down1_df = day_df
     down1_df = down1_df.sort_values('D_C_MIN', ascending=True)
-    return up_df, down_df, up1_df, down1_df, table_data
+    down1_df.set_index('symbol', inplace=True)
+    return up_df, down_df, up1_df, down1_df, table_data, actual_df
     # print(pyfiglet.figlet_format("UP", font="slant"))
     # print(tabulate(up_df.head(20), headers='keys', tablefmt='psql'))
     # print(pyfiglet.figlet_format("DOWN", font="slant"))
@@ -162,13 +183,47 @@ def get_data(new_data=False):
         return previous_day_df
 
 
-print(pyfiglet.figlet_format("Binance Bot"))
 previous_day_df = get_data()
 
+
 while True:
-    up_df, down_df, up1_df, down1_df, table_data = get_tickers(
+    up_df, down_df, up1_df, down1_df, table_data, actual_df = get_tickers(
         partha_account, previous_day_df)
-    table_placeholder.dataframe(table_data)
+
+   # Sort the dataframes
+    up = actual_df[actual_df['D_CHANGE'] > 0]
+    up = up.sort_values('D_CHANGE', ascending=False)
+    down = actual_df[actual_df['D_CHANGE'] < 0]
+    down = down.sort_values('D_CHANGE', ascending=True)
+
+    up.set_index('symbol', inplace=True)
+    down.set_index('symbol', inplace=True)
+
+    # Display the dataframes in their respective placeholders
+
+    up_placeholder.dataframe(up.head(20))
+
+    down_placeholder.dataframe(down.head(20))
+
+    up1_placeholder.dataframe(up1_df.head(20))
+
+    down1_placeholder.dataframe(down1_df.head(20))
+
+    def get_chart(coin):
+        # Code to get the chart of the given coin
+        pass
+
+    def on_button_click():
+        coin = st.text_input("Enter coin symbol:")
+        if coin:
+            get_chart(coin)
+
+    button_col, input_col = st.columns(2)
+    with button_col:
+        st.button("Get Chart", on_button_click)
+
+    with input_col:
+        st.empty()
 
     # Wait for 5 seconds
     time.sleep(5)
